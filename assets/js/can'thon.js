82fc23b5053192df89f5795aa8eb59d0689c09aa
vincent <lavallee.vincent@protonmail.com>
2018-11-12 21:18:34 +0100

    ajout projet DevCone

---
+++ assets/js/can'thon.js
@@ -0,0 +1,11 @@
+//compteur de point
+$("#playerOne").click(function(){
+result=$('#count1').val();
+result++;
+  $('#count1').val(result);
+});
+$("#playerTwo").click(function(){
+result=$('#count2').val();
+result++;
+  $('#count2').val(result);
+});
